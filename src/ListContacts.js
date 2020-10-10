4f7af262cc6a10e218e5161bda9cfd886f241f6c
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-10 16:36:59 +0530

    Add 'Now showing' section to ListContacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
         })
     }
 
+    clearQuery = () => {
+        this.updateQuery('')
+    }
+
     render() {
         const { query } = this.state
         const { contacts, onDeleteContact } = this.props
@@ -35,6 +39,13 @@ class ListContacts extends Component {
                         value={query}
                         onChange={(event) => {this.updateQuery(event.target.value)}}/>
                 </div>
+
+                {showingContacts.length !== contacts.length && (
+                    <div className="showing-contacts">
+                        <span>Now showing {showingContacts.length} of {contacts.length}</span>
+                        <button onClick={() => this.clearQuery()}>Show all</button>
+                    </div>
+                )}
                 <ol className="contact-list">
                     {showingContacts.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>

e9ee425266c300f10fd87c6bfd416861c6d2c71f
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-10 16:11:39 +0530

    Convert ListContacts to controlled component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,30 +1,58 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts(props) {
-    return (
-        <ol className="contact-list">
-            {props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item'>
-                    <div 
-                        className="contact-avatar"
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}>
-                    </div>
-                    <div className="contact-details">
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
-                    <button
-                        className="contact-remove"
-                        onClick = {() => props.onDeleteContact(contact)}>
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({
+            query: query.trim()
+        })
+    }
+
+    render() {
+        return (
+            <div className="list-contacts">
+                {JSON.stringify(this.state)}
+                <div className="list-contacts-top">
+                    <input
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search Contacts"
+                        value={this.state.query}
+                        onChange={(event) => {this.updateQuery(event.target.value)}}/>
+                </div>
+                <ol className="contact-list">
+                    {this.props.contacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div 
+                                className="contact-avatar"
+                                style={{
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }}>
+                            </div>
+                            <div className="contact-details">
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button
+                                className="contact-remove"
+                                onClick = {() => this.props.onDeleteContact(contact)}>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
 
 ListContacts.propTypes = {

d3a2a327118fc84d04af3f7fb0196ef2d31694e4
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-11 19:00:35 +0530

    Remove contact from server

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter(c => c.id !== contact.id)
     }))
+    ContactsAPI.remove(contact)
   }
 
   render() {

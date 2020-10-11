854948395846adb1ff3fce9b9ce0b1da126e3352
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-11 18:56:29 +0530

    Fetch contacts from server

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
-import ListContacts from './ListContacts'
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   }
 
-  // constructor(props) {
-  //   super(props)
-  //   this.removeContact = this.removeContact.bind(this)
-  // }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
 
   removeContact = (contact) => {
     this.setState((currentState) => ({
